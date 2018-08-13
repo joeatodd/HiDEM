04c3aaf3959b2950fdd22d8db799d8a77b9eeb92
J Todd <jat39@st-andrews.ac.uk>
2018-08-13 18:47:12 +0100

    update cray compilation script

--- compile.sh
+++ compile.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-## This is a sample compilation script for CRAY systems
-# -R b  for array bounds checking
-
-module swap PrgEnv-gnu PrgEnv-cray
-cd ./src/
-ftn -o ../HiDEM -O3  typedefs.f90 io.f90 dist.f90 circ.f90 effload.f90 amat.f tmat.f ttmat.f kmat.f glas.f90 ranmar.f dt.f90 wave2.f90
-cd ..
-rm INOUT.mod
-rm TYPEDEFS.mod
-module swap PrgEnv-cray PrgEnv-gnu
+scripts/compilation/compile_cray.sh
--- scripts/compilation/compile_cray.sh
+++ scripts/compilation/compile_cray.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 module swap PrgEnv-gnu PrgEnv-cray
+module load metis
 cd ./src/
-ftn -o ../HiDEM -O3 typedefs.f90 utils.f90 io.f90 octree.F90 glas.f90 dist.f90 effload.f90 amat.f tmat.f ttmat.f kmat.f ranmar.f wave2.f90
+ftn -o ../HiDEM -O3 -lmetis typedefs.f90 utils.f90 io.f90 octree.F90 glas.f90 dist.f90 effload.f90 amat.f tmat.f ttmat.f kmat.f ranmar.f wave2.f90
+#ftn -o ../HiDEM -g -R bcp -lmetis  typedefs.f90 utils.f90 io.f90 octree.F90 glas.f90 dist.f90 effload.f90 amat.f tmat.f ttmat.f kmat.f ranmar.f wave2.f90
 cd ..
 rm INOUT.mod
 rm TYPEDEFS.mod
 rm LATTICE.mod
 rm UTILS.mod
 rm OCTREE.mod
+rm EFFL.mod
 module swap PrgEnv-cray PrgEnv-gnu
