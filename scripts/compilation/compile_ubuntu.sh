3a38ded19b68956528460399d9c7467bc8f26ed2
J Todd <jat39@st-andrews.ac.uk>
2018-09-18 12:51:06 +0100

    Change default permissions on installation scripts, and default to install HiDEM in repo directory

--- scripts/compilation/compile_ubuntu.sh
+++ scripts/compilation/compile_ubuntu.sh
@@ -1,7 +1,7 @@
 mkdir -p build #make dir if first build
 cd build
 
-cmake ../ -DCMAKE_INSTALL_PREFIX="/usr/local/" -DCMAKE_TOOLCHAIN_FILE=./scripts/toolchains/HiDEM-ubuntu.cmake
+cmake ../ -DCMAKE_INSTALL_PREFIX="." -DCMAKE_TOOLCHAIN_FILE=./scripts/toolchains/HiDEM-ubuntu.cmake
 
 #If you want a debug build instead:
 #cmake ../ -DCMAKE_INSTALL_PREFIX="/usr/local/" -DCMAKE_TOOLCHAIN_FILE=./scripts/toolchains/HiDEM-ubuntu.cmake -DCMAKE_BUILD_TYPE=Debug
